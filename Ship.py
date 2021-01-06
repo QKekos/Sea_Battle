3ffb43d512e345c9e719528954a06112442caecd
QKekos <daniil.run355@gmail.com>
2021-01-06 12:17:27 +0200

    Ship now use Dot objects in init

--- Ship.py
+++ Ship.py
@@ -2,55 +2,77 @@ from Exceptions import NonExistingShipError
 from Dot import Dot
 
 
-
 class Ship:
 
     def __init__(self, start_point, end_point=None):
 
-        self.start_point = None
-        self.end_point = None
+        if not isinstance(start_point, Dot):
+            if isinstance(start_point, tuple):
+                start_point = Dot(start_point[0], start_point[1])
+
+        if end_point is not None and not isinstance(end_point, Dot):
+            if isinstance(end_point, tuple):
+                end_point = Dot(end_point[0], end_point[1])
 
         if end_point is None:
             self.start_point = self.end_point = start_point
             self._health = 1
 
         else:
-
-            for i in range(2):
-
-                if start_point[i] == end_point[i]:
-
-                    if start_point[i-1] >= end_point[i-1]:
-
-                        self.start_point = end_point
-                        self.end_point = start_point
-                        self._health = self.end_point[i-1] - self.start_point[i-1] + 1
-
-                        break
-
-                    else:
-
-                        self.start_point = start_point
-                        self.end_point = end_point
-                        self._health = self.end_point[i-1] - self.start_point[i-1] + 1
-
-                        break
-
-        if self.start_point is None:
-            raise NonExistingShipError
+            self.set_minmax_dots(start_point, end_point)
 
         self.dots = []
         self.create_dots_array()
 
+    def set_minmax_dots(self, first_dot, second_dot):
+
+        if first_dot.x == second_dot.x:
+
+            if first_dot.y >= second_dot.y:
+                
+                self.start_point = Dot(first_dot.x, second_dot.y)
+                self.end_point = Dot(first_dot.x, first_dot.y)
+
+                self.health = first_dot.y - second_dot.y + 1
+                return
+
+            else:
+                
+                self.start_point = Dot(first_dot.x, first_dot.y)
+                self.end_point = Dot(first_dot.x, second_dot.y)
+
+                self.health = second_dot.y - first_dot.y + 1
+                return
+
+        elif first_dot.y == second_dot.y:
+
+            if first_dot.x >= second_dot.x:
+                
+                self.start_point = Dot(second_dot.x, first_dot.y)
+                self.end_point = Dot(first_dot.x, first_dot.y)
+
+                self.health = first_dot.x - second_dot.x + 1
+                return
+
+            else:
+                
+                self.start_point = Dot(first_dot.x, first_dot.y)
+                self.end_point = Dot(second_dot.x, first_dot.y)
+
+                self.health = second_dot.x - first_dot.x + 1
+                return
+
+        raise NonExistingShipError
+
     def create_dots_array(self):
 
-        if self.start_point[0] == self.end_point[0]:  # Horizontal
-            for i in range(self.start_point[1], self.end_point[1] + 1):
-                self.dots.append(Dot(self.start_point[0], i))
+        if self.start_point.x == self.end_point.x:  # Horizontal
+            for new_y in range(self.start_point.y, self.end_point.y + 1):
+                self.dots.append(Dot(self.start_point.x, new_y))
 
         else:  # Vertical
-            for i in range(self.start_point[0], (self.end_point[0]) + 1):
-                self.dots.append(Dot(i, self.start_point[1]))
+            for new_x in range(self.start_point.x, self.end_point.x + 1):
+                self.dots.append(Dot(new_x, self.start_point.y))
 
     def __repr__(self):
         return f'{self.health}: {self.dots}'
